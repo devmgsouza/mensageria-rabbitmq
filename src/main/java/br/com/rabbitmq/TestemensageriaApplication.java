6fbe209defc31b24325275b34134a1638e91c863
Marcio Galvão <dev.mgsouza@gmail.com>
2019-11-01 22:35:34 -0300

    Upload project

---
+++ src/main/java/br/com/rabbitmq/TestemensageriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestemensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestemensageriaApplication.class, args);
+	}
+
+}
